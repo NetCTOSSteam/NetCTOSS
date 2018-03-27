8c1f4456afac94399f257fd12e810b5569f77b96
asus <asus@DESKTOP-MP7CLVR>
2018-03-27 16:23:12 +0800

    melaBean 添加一个字段

--- NetCTOSS/src/main/java/com/alibaba/NetCTOSS/usermag/service_demand/IMealDemandService.java
+++ NetCTOSS/src/main/java/com/alibaba/NetCTOSS/usermag/service_demand/IMealDemandService.java
@@ -1,5 +1,6 @@
 package com.alibaba.NetCTOSS.usermag.service_demand;
 
+import com.github.pagehelper.Page;
 
 /**
  * 资费 的查询接口
@@ -7,8 +8,11 @@ package com.alibaba.NetCTOSS.usermag.service_demand;
  *
  */
 public interface IMealDemandService {
-	
-	
+	/**
+	 * 初始页面的分页查询
+	 * @return 分页对象
+	 */
+	public Page<?> findAllAndPage();
 	
 	
 	
