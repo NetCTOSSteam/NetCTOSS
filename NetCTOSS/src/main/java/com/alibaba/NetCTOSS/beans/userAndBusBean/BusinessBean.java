b62b21f98f29b8a97cd5c4a41636f215452dee38
Administrator <Administrator@8YLDW937WI7LOY3>
2018-03-27 13:35:33 +0800

    账务账号    业务账号    实体Bean映射

--- NetCTOSS/src/main/java/com/alibaba/NetCTOSS/beans/userAndBusBean/BusinessBean.java
+++ NetCTOSS/src/main/java/com/alibaba/NetCTOSS/beans/userAndBusBean/BusinessBean.java
@@ -1,12 +1,28 @@
 package com.alibaba.NetCTOSS.beans.userAndBusBean;
 
 import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.GenericGenerator;
 
 /**
  * 业务 账户 类
  * @author Administrator
  *
  */
+@Entity
+@Table(name="t_business",catalog="netctoss")
 public class BusinessBean implements Serializable {
 
 	/**
@@ -14,4 +30,180 @@ public class BusinessBean implements Serializable {
 	 */
 	private static final long serialVersionUID = -2673540182928695962L;
 
+	/**
+	 * bus_id
+	 * id字段
+	 */
+	@Id
+	@Column(name="bus_id")
+	@GenericGenerator(name="hibernate.id",strategy="identity")
+	@GeneratedValue(generator="hibernate.id")
+	private Integer id;
+	
+	/**
+	 * bus_name
+	 * 业务账户名
+	 */
+	@Column(name="bus_name",length=20)
+	private String busName;
+	
+	/**
+	 * bus_ip
+	 * 实验室服务器IP
+	 */
+	@Column(name="bus_ip",length=20)
+	private String serverIP;
+	
+	
+	/**
+	 * bus_password
+	 * 密码
+	 */
+	@Column(name="bus_password",length=32)
+	private String password;
+	
+	/**
+	 * bus_mea_id
+	 * 资费外键
+	 */
+	@ManyToOne(fetch=FetchType.LAZY)
+	@Cascade(value= {CascadeType.ALL})
+	@JoinColumn(name="bus_mea_id")
+	private MealBean mealBean;
+	
+	/**
+	 * bus_u_id
+	 * 账务账户外键
+	 */
+	@ManyToOne(fetch=FetchType.LAZY)
+	@Cascade(value= {CascadeType.ALL})
+	@JoinColumn(name="bus_u_id")
+	private UserBean userBean;
+	
+	/**
+	 * bus_state
+	 * 业务状态
+	 */
+	@Column(name="bus_state")
+	private boolean state;
+	
+	/**
+	 * bus_next_mea_id
+	 * 下月资费外键
+	 */
+	@ManyToOne(fetch=FetchType.LAZY)
+	@Cascade(value= {CascadeType.ALL})
+	@JoinColumn(name="bus_next_mea_id")
+	private MealBean nextMealBean;
+	
+	/**
+	 * bus_time
+	 * 开通时间
+	 */
+	@Column(name="bus_time")
+	private Date startTime;
+	
+	
+	public BusinessBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getBusName() {
+		return busName;
+	}
+
+
+	public void setBusName(String busName) {
+		this.busName = busName;
+	}
+
+
+	public String getServerIP() {
+		return serverIP;
+	}
+
+
+	public void setServerIP(String serverIP) {
+		this.serverIP = serverIP;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public MealBean getMealBean() {
+		return mealBean;
+	}
+
+
+	public void setMealBean(MealBean mealBean) {
+		this.mealBean = mealBean;
+	}
+
+
+	public UserBean getUserBean() {
+		return userBean;
+	}
+
+
+	public void setUserBean(UserBean userBean) {
+		this.userBean = userBean;
+	}
+
+
+	public boolean isState() {
+		return state;
+	}
+
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
+
+
+	public MealBean getNextMealBean() {
+		return nextMealBean;
+	}
+
+
+	public void setNextMealBean(MealBean nextMealBean) {
+		this.nextMealBean = nextMealBean;
+	}
+
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+
+	@Override
+	public String toString() {
+		return "BusinessBean [id=" + id + ", busName=" + busName + ", serverIP=" + serverIP + ", password=" + password
+				+ ", mealBean=" + mealBean + ", userBean=" + userBean + ", state=" + state + ", nextMealBean="
+				+ nextMealBean + ", startTime=" + startTime + "]";
+	}
+	
 }
--- NetCTOSS/src/main/java/com/alibaba/NetCTOSS/beans/userAndBusBean/UserBean.java
+++ NetCTOSS/src/main/java/com/alibaba/NetCTOSS/beans/userAndBusBean/UserBean.java
@@ -2,16 +2,191 @@ package com.alibaba.NetCTOSS.beans.userAndBusBean;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 /**
  * 账务账户类
  * @author Administrator
  *
  */
+
+@Entity
+@Table(name="t_user",catalog="netctoss")
 public class UserBean implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2056072704313750420L;
+	
+	
+	/**
+	 *  id字段
+	 */
+	@Id
+	@Column(name="id")
+	@GenericGenerator(name="hibernate.id",strategy="identity")
+	@GeneratedValue(generator="hibernate.id")
+	private Integer id;
+	
+	/**
+	 * u_name
+	 * 账务名字
+	 */
+	@Column(name="u_name",length=20)
+	private String userName;
+	
+	/**
+	 * u_tel
+	 * 电话
+	 */
+	@Column(name="u_tel",length=11)
+	private String tel;
+	
+	/**
+	 * id_card
+	 * 身份证
+	 */
+	@Column(name="id_card",length=20)
+	private String idCard;
+	
+	/**
+	 * u_gender
+	 * 性别
+	 */
+	@Column(name="u_gender")
+	private Integer gender;
+	
+	/**
+	 * u_address
+	 * 通讯地址
+	 */
+	@Column(name="u_address",length=100)
+	private String address;
+	
+	
+	/**
+	 * u_postcode
+	 * 邮编
+	 */
+	@Column(name="u_postcode")
+	private Integer postcode;
+	/**
+	 * u_qq
+	 * QQ号码
+	 */
+	@Column(name="u_qq",length=15)
+	private String qq;
+	
+	/**
+	 * login_name
+	 * 账户登陆名
+	 */
+	@Column(name="login_name",length=20)
+	private String loginName;
+	
+	/**
+	 * pass_word
+	 * 登陆密码
+	 */
+	@Column(name="pass_word",length=32)
+	private String password;
+	
+	public UserBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getIdCard() {
+		return idCard;
+	}
+
+	public void setIdCard(String idCard) {
+		this.idCard = idCard;
+	}
+
+	public Integer getGender() {
+		return gender;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Integer getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(Integer postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getQq() {
+		return qq;
+	}
+
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserBean [id=" + id + ", userName=" + userName + ", tel=" + tel + ", idCard=" + idCard + ", gender="
+				+ gender + ", address=" + address + ", postcode=" + postcode + ", qq=" + qq + ", loginName=" + loginName
+				+ ", password=" + password + "]";
+	}
 
 }
