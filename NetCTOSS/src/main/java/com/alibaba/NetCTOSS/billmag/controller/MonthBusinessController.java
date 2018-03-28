3c2791d4c1e972d45458c448fb6cdddf53b40902
Administrator <Administrator@8YLDW937WI7LOY3>
2018-03-28 19:38:46 +0800

    增加billMag包下面的controller包   里面增加了两个 页面控制器类 MonthAccController.java MonthBusinessController.java

---
+++ NetCTOSS/src/main/java/com/alibaba/NetCTOSS/billmag/controller/MonthBusinessController.java
@@ -0,0 +1,10 @@
+package com.alibaba.NetCTOSS.billmag.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/monthBusiness")
+public class MonthBusinessController {
+
+}
