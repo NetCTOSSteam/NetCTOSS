cb260fb3c08cef124e0bfc3e9748ac56967b504a
litcwy <806078897@qq.com>
2018-03-28 10:55:07 +0800

    提交管理员相关mapper映射文件

--- NetCTOSS/src/main/java/com/alibaba/NetCTOSS/admmag/mapper_demand/RoleMapper.java
+++ NetCTOSS/src/main/java/com/alibaba/NetCTOSS/admmag/mapper_demand/RoleMapper.java
@@ -1,5 +1,14 @@
 package com.alibaba.NetCTOSS.admmag.mapper_demand;
 
+import java.util.Set;
+
+import org.springframework.data.repository.query.Param;
+
 public interface RoleMapper {
-	
+	/**
+	 * 通过角色名查询权限信息
+	 * @param roleName
+	 * @return
+	 */
+	public Set<String> getPermissions(@Param("roleName")String roleName);
 }
