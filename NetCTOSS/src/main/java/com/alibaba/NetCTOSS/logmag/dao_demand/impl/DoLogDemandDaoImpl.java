e012631838c9d1df1c4696e1f544e5a350908547
Shy <Shy@DESKTOP-GSMQNK1>
2018-03-28 15:53:31 +0800

    操作日志实现

--- NetCTOSS/src/main/java/com/alibaba/NetCTOSS/logmag/dao_demand/impl/DoLogDemandDaoImpl.java
+++ NetCTOSS/src/main/java/com/alibaba/NetCTOSS/logmag/dao_demand/impl/DoLogDemandDaoImpl.java
@@ -1,7 +1,31 @@
 package com.alibaba.NetCTOSS.logmag.dao_demand.impl;
 
-import com.alibaba.NetCTOSS.logmag.dao_demand.IDoLogDemandDao;
+import java.util.List;
+import java.util.Map;
 
-public class DoLogDemandDaoImpl implements IDoLogDemandDao {
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Repository;
+
+import com.alibaba.NetCTOSS.beans.logBean.DoLogBean;
+import com.alibaba.NetCTOSS.logmag.dao_demand.IDoLogDemandDao;
+import com.alibaba.NetCTOSS.logmag.mapper_demand.DoLogMapper;
+
+@Repository
+public class DoLogDemandDaoImpl implements IDoLogDemandDao{
+	@Resource
+	private DoLogMapper dg;
+
+	@Override
+	public List<DoLogBean> findAll() {
+		// TODO Auto-generated method stub
+		return dg.findAll();
+	}
+
+	@Override
+	public List<DoLogBean> findDoLogsByParams(Map params) {
+		// TODO Auto-generated method stub
+		return dg.findDoLogsByParams(params);
+	}	
 
 }
