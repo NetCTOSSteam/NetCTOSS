e33c91df9f25ac78e506583f3f4a0c740fcf02b5
Shy <Shy@DESKTOP-GSMQNK1>
2018-03-29 10:35:58 +0800

    上传了登录日志相关内容

--- NetCTOSS/src/main/java/com/alibaba/NetCTOSS/logmag/dao_demand/impl/LoginExitDemandDaoImpl.java
+++ NetCTOSS/src/main/java/com/alibaba/NetCTOSS/logmag/dao_demand/impl/LoginExitDemandDaoImpl.java
@@ -1,7 +1,32 @@
 package com.alibaba.NetCTOSS.logmag.dao_demand.impl;
 
-import com.alibaba.NetCTOSS.logmag.dao_demand.ILoginExitDemandDao;
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Repository;
+
+import com.alibaba.NetCTOSS.beans.logBean.LoginExitBean;
+import com.alibaba.NetCTOSS.logmag.dao_demand.ILoginExitDemandDao;
+import com.alibaba.NetCTOSS.logmag.mapper_demand.LoginExitMapper;
+
+@Repository
 public class LoginExitDemandDaoImpl implements ILoginExitDemandDao {
+
+	@Resource
+	private LoginExitMapper le;
+	
+	@Override
+	public List<LoginExitBean> findAll() {
+		// TODO Auto-generated method stub
+		return le.findAll();
+	}
+
+	@Override
+	public List<LoginExitBean> findLoginExitsByParams(Map params) {
+		// TODO Auto-generated method stub
+		return le.findLoginExitsByParams(params);
+	}
 
 }
