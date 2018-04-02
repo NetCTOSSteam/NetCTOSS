68ce73a3ad30481c7baae7adc7acfc17aaef19a0
litcwy <806078897@qq.com>
2018-04-02 11:39:11 +0800

    更改Userrealm

--- NetCTOSS/src/main/java/com/alibaba/NetCTOSS/usermag/realm/UserRealm.java
+++ NetCTOSS/src/main/java/com/alibaba/NetCTOSS/usermag/realm/UserRealm.java
@@ -65,7 +65,6 @@ public class UserRealm extends AuthorizingRealm {
 		String loginName = (String) customizedToken.getPrincipal();
 
 		UserBean user = userDemandServiceImpl.findByLoginName(loginName);
-		System.out.println(user);
 		if (user != null) {
 			AuthenticationInfo authcInfo = new SimpleAuthenticationInfo(user.getLoginName(), user.getPassword(),
 					getName());
