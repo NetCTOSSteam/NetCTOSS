ae884900870a0db889590ce2cc70e1ca72cdfba8
Shy <Shy@DESKTOP-GSMQNK1>
2018-04-03 17:12:14 +0800

    登录退出日志的实现

---
+++ NetCTOSS/src/main/java/com/alibaba/NetCTOSS/Interceptor/LogInterceptor.java
@@ -0,0 +1,100 @@
+package com.alibaba.NetCTOSS.Interceptor;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.SessionListener;
+import org.apache.shiro.subject.Subject;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.NetCTOSS.admmag.service_demand.IAdminDemandService;
+import com.alibaba.NetCTOSS.beans.admAndRoleBean.AdministratorBean;
+import com.alibaba.NetCTOSS.beans.logBean.LoginExitBean;
+import com.alibaba.NetCTOSS.beans.userAndBusBean.UserBean;
+import com.alibaba.NetCTOSS.logmag.service_handle.ILoginExitHandleService;
+import com.alibaba.NetCTOSS.usermag.service_demand.IUserDemandService;
+
+public class LogInterceptor implements HandlerInterceptor,SessionListener{
+	
+	@Resource
+	private ILoginExitHandleService loginExitHandleServiceImpl;
+	
+	@Resource
+	private IAdminDemandService adminDemandServiceImpl;
+	
+	@Resource
+	private IUserDemandService userDemandServiceImpl;
+
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("MyFirstInterceptor-----preHandle()");
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		Subject subject=SecurityUtils.getSubject();
+		Session session=subject.getSession();
+		AdministratorBean adm=null;
+		UserBean user=null;
+		LoginExitBean login = new LoginExitBean();
+		if(session.getAttribute("user")==null) {
+			adm=(AdministratorBean) session.getAttribute("admin");
+			//得到管理员登录名
+			String adminLoginName = adm.getLoginName();
+			//通过管理员登录名查询管理员对象
+			AdministratorBean admin = adminDemandServiceImpl.findAdminByAdminLoginName(adminLoginName);
+			login.setName(admin.getAdminName());//管理员名字
+			login.setAccNumber("管理员："+adminLoginName);//管理员登录名
+		}else {
+			user=(UserBean) session.getAttribute("user");
+			//得到用户登录名
+			String loginName = user.getLoginName();
+			//通过管理员登录名查询管理员对象
+			UserBean newUser = userDemandServiceImpl.findByLoginName(loginName);
+			
+			login.setName(newUser.getUserName());//用户的名字
+			
+			login.setAccNumber("用户："+user.getLoginName());
+		}
+		login.setIP(session.getHost());
+		login.setLoginTime(new Date());
+		loginExitHandleServiceImpl.saveLoginExitBean(login);
+	
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onExpiration(Session arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(Session arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStop(Session session) {
+	}
+}
--- NetCTOSS/src/main/java/com/alibaba/NetCTOSS/filter/SystemLogoutFilter.java
+++ NetCTOSS/src/main/java/com/alibaba/NetCTOSS/filter/SystemLogoutFilter.java
@@ -1,16 +1,37 @@
 package com.alibaba.NetCTOSS.filter;
 
+import java.util.Date;
+
+import javax.annotation.Resource;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.apache.shiro.session.Session;
 import org.apache.shiro.session.SessionException;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.LogoutFilter;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.NetCTOSS.admmag.service_demand.IAdminDemandService;
+import com.alibaba.NetCTOSS.beans.admAndRoleBean.AdministratorBean;
+import com.alibaba.NetCTOSS.beans.logBean.LoginExitBean;
+import com.alibaba.NetCTOSS.beans.userAndBusBean.UserBean;
+import com.alibaba.NetCTOSS.logmag.service_handle.ILoginExitHandleService;
+import com.alibaba.NetCTOSS.usermag.service_demand.IUserDemandService;
+
 @Service  
 public class SystemLogoutFilter extends LogoutFilter {  
+	
+	@Resource
+	private ILoginExitHandleService loginExitHandleServiceImpl;
+
+	@Resource
+	private IAdminDemandService adminDemandServiceImpl;
+
+	@Resource
+	private IUserDemandService userDemandServiceImpl;
+	
     @Override  
     protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {  
         //在这里执行退出系统前需要清空的数据  
@@ -18,7 +39,34 @@ public class SystemLogoutFilter extends LogoutFilter {
         String redirectUrl=getRedirectUrl(request,response,subject);  
         ServletContext context= request.getServletContext();  
         try {  
-            subject.logout();  
+            Session session = subject.getSession();
+    		AdministratorBean adm = null;
+    		UserBean user = null;
+    		LoginExitBean login = new LoginExitBean();
+    		if (session.getAttribute("user") == null) {
+    			adm = (AdministratorBean) session.getAttribute("admin");
+    			// 得到管理员登录名
+    			String adminLoginName = adm.getLoginName();
+    			// 通过管理员登录名查询管理员对象
+    			AdministratorBean admin = adminDemandServiceImpl.findAdminByAdminLoginName(adminLoginName);
+    			login.setName(admin.getAdminName());// 管理员名字
+    			login.setAccNumber("管理员：" + adminLoginName);// 管理员登录名
+    		} else {
+    			user = (UserBean) session.getAttribute("user");
+    			// 得到用户登录名
+    			String loginName = user.getLoginName();
+    			// 通过管理员登录名查询管理员对象
+    			UserBean newUser = userDemandServiceImpl.findByLoginName(loginName);
+
+    			login.setName(newUser.getUserName());// 用户的名字
+
+    			login.setAccNumber("用户：" + user.getLoginName());
+    		}
+    		login.setIP(session.getHost());
+    		login.setQuitTime(new Date());
+    		loginExitHandleServiceImpl.saveLoginExitBean(login);
+            
+    		subject.logout();
             context.removeAttribute("error");  
         }catch (SessionException e){  
             e.printStackTrace();  
